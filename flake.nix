62b098ee8a810bdb805194bffaccdd5465cff93b
Karol Skoczyk <send@qrxnz.dev>
2025-05-03 11:52:26 +0200

    DevShell Update

--- flake.nix
+++ flake.nix
@@ -1,25 +1,28 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = {
     self,
     nixpkgs,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.x86_64-linux.default =
-      pkgs.mkShell
-      {
-        nativeBuildInputs = with pkgs; [
-          # Formatters
-          treefmt2
-          mdformat
-          alejandra
-        ];
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            # Formatters
+            treefmt2
+            mdformat
+            alejandra
+          ];
+        };
+      }
+    )
+    // {
+      overlays.default = self: pkgs: {
+        hello = self.packages."${pkgs.system}".hello;
       };
-  };
+    };
 }
